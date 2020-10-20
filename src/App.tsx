6316e3a1bb0ba0d8d635927468567955e57490d1
Your Name <you@example.com>
2020-10-20 22:54:19 +0700

    add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Input, Button } from "antd";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="form-group mb-1">
+        <label htmlFor="">Reason</label>
+        <Input />
+      </div>
+      <div className="form-group mb-1">
+        <label htmlFor="">Price</label>
+        <Input />
+      </div>
+      <div className="form-group">
+        <Button type="primary">Save changes</Button>
+      </div>
+
+      <table className="table mt-1">
+        <thead>
+          <tr>
+            <th>date</th>
+            <th>reason</th>
+            <th>price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
